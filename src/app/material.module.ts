6a05e95bf0ecee89349e94eb0fdb59beb27c5289
Leonardo Adell <leoadell@gmail.com>
2018-12-22 08:41:17 -0300

    navbar

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
 
 import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @NgModule({
   imports: [
-    MatButtonModule
+    MatButtonModule, MatToolbarModule
   ],
   exports: [
-    MatButtonModule
+    MatButtonModule, MatToolbarModule
   ]
 })
 export class MaterialModule { }
