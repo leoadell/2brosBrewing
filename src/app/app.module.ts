707e3228f6ecd27e14507eaf199f2782e37ca5cd
Leonardo Adell <leoadell@gmail.com>
2018-12-21 21:09:04 -0300

    angular modules added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,28 @@ import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
 import { SharedModule } from './shared/shared.module';
 
+
+/************************************************ */
+/*Firebae modules */
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { AngularFireStorageModule, AngularFireStorage } from '@angular/fire/storage';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+
+
+/************************************************ */
+import { environment } from 'src/environments/environment';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFirestoreModule,
+    AngularFireAuthModule,
+    AngularFireStorageModule,
     CoreModule,
     SharedModule
   ],
