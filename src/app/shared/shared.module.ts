9da308bce7696ca7f95d967d7c101903944857c3
Leonardo Adell <leoadell@gmail.com>
2018-12-22 10:41:43 -0300

    auth, login y logout

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,17 +1,13 @@
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
 import { CommonModule } from '@angular/common';
 import { MaterialModule } from '../material.module';
 import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
-  imports: [
-    CommonModule,
-    MaterialModule
-  ],
-  exports: [
-    CommonModule,
-    MaterialModule, NavbarComponent
-  ],
+  imports: [CommonModule, MaterialModule, RouterModule],
+  exports: [CommonModule, MaterialModule, NavbarComponent, RouterModule],
   declarations: [NavbarComponent]
 })
 export class SharedModule { }
