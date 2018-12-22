981b06f15a7a2c7322c51d30df308e0425f68378
Leonardo Adell <leoadell@gmail.com>
2018-12-22 08:14:36 -0300

    material and browser animation module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+/************************************************ */
+/**Angular Material Modules */
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+/************************************************ */
+
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
 import { SharedModule } from './shared/shared.module';
